ac3eeb2c55d9d2bcdf1ef1192e0c47ff366d32c1
laristizabala <laura.aristizabal@guane.com.co>
2025-05-02 11:07:12 -0500

    :sparkles: marketView implemented

--- src/views/home/MarketView.vue
+++ src/views/home/MarketView.vue
@@ -1,7 +1,114 @@
 <template>
-  <div>Hola2</div>
+  <div class="market-main">
+    <h1 class="market-main__title">🎣 Market <span>Fishing Game</span></h1>
+
+    <div class="market-card">
+      <div class="market-card-header">
+        <p v-for="item in tableSchema" :key="item.prop" class="market-card-header__title">
+          {{ item.label }}
+        </p>
+      </div>
+      <div class="market-card-body" v-for="item in market?.items" :key="item.id">
+        <!-- <p class="market-card-body__text">
+          {{ item.id }}
+        </p> -->
+        <p class="market-card-body__text">
+          {{ item.name }}
+        </p>
+        <p class="market-card-body__text">
+          {{ item.type }}
+        </p>
+        <p class="market-card-body__text">
+          {{ item.description }}
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
 
-<style lang="scss" scoped></style>
+const market = ref([])
+const loading = ref(false)
+const tableSchema = ref([
+  // { label: 'Id', prop: 'id' },
+  { label: 'Name', prop: 'name' },
+  { label: 'Type', prop: 'type' },
+  { label: 'Description', prop: 'description' },
+])
+
+const getMarketData = async () => {
+  try {
+    const response = await fetch('https://api-game.bloque.app/game/market')
+    if (!response.ok) throw new Error(`Error ${response.status}`)
+    market.value = await response.json()
+  } catch (error) {
+    error.value = error.message
+  } finally {
+    loading.value = false
+  }
+}
+
+onMounted(() => {
+  getMarketData()
+})
+</script>
+
+<style lang="scss" scoped>
+.market-main {
+  margin: 2rem;
+  &__title {
+    color: $color-primary-background;
+    span {
+      color: $color-secondary-background;
+    }
+  }
+}
+.market-card-header {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr) 2fr;
+  grid-template-rows: 1fr;
+  grid-column-gap: 10px;
+  grid-row-gap: 0px;
+  border: 2px solid $color-secondary-background;
+  padding: 10px 0 10px 10px;
+  border-radius: 20px;
+  margin: 10px 0 15px;
+  &__title {
+    font-size: 14px;
+    font-weight: bold;
+    color: $color-secondary-background;
+  }
+}
+.market-card-body {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr) 2fr;
+  grid-template-rows: 1fr;
+  grid-column-gap: 10px;
+  grid-row-gap: 0px;
+  border-bottom: 1px solid $color-gray-hr;
+  padding: 10px 0 10px 14px;
+  // border-radius: 20px;
+  margin: 10px 0;
+  &__text {
+    display: flex;
+    align-items: center;
+    color: $color-text-soft;
+    font-size: 12px;
+  }
+  &__rank {
+    background-color: $color-secondary-background;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 40px;
+    width: 35px;
+    height: 35px;
+    padding: 2px;
+    margin: 2px;
+    color: $color-white;
+  }
+}
+</style>
