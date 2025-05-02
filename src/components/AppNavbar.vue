a3c65aaeba48be57ad103a146d84d145ddbcc7d2
laristizabala <laura.aristizabal@guane.com.co>
2025-05-02 11:03:01 -0500

    :sparkles: appNavbar implemented and styles improved

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,39 +1,41 @@
 <template>
-  <header>
-    <nav>
-      <ul class="nav-app-container">
-        <li v-for="item in navItems" :key="item.id">
-          <button @click="setActive(item.id)" class="nav-app-container__option">
-            {{ item.label }}
-          </button>
-        </li>
-      </ul>
-      <!-- <button
-        @click="setActive('leaderboard')"
-        :class="[
-          'px-4 py-2 rounded-md focus:outline-none',
-          isActive === item.id
-            ? 'bg-primary text-white'
-            : 'text-gray-700 hover:bg-gray-200 hover:text-gray-900',
-        ]"
-      >
-        <RouterLink to="/">Leaderboard</RouterLink>
-      </button>
-      <button>
-        <RouterLink to="/market">Market</RouterLink>
-      </button> -->
-    </nav>
-  </header>
+  <nav class="navbar-app">
+    <ul class="nav-app-container">
+      <li v-for="item in navItems" :key="item.id" class="nav-app-container__option">
+        <router-link
+          @click="setActive(item.id)"
+          :to="`/${item.id}`"
+          class="nav-app-container__link"
+          :class="
+            isActive === item.id ? `nav-app-container__link nav-app-container__link--active` : ''
+          "
+          ><IconRank
+            v-if="item.id === ''"
+            class="icon-rank"
+            :class="isActive === item.id ? 'icon-rank icon-rank--active' : ''"
+          ></IconRank>
+          <IconMarket
+            v-else
+            class="icon-rank"
+            :class="isActive === item.id ? 'icon-rank icon-rank--active' : ''"
+          ></IconMarket>
+          {{ item.label }}</router-link
+        >
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import IconRank from './icons/IconRank.vue'
+import IconMarket from './icons/IconMarket.vue'
 
 const navItems = ref([
+  { id: '', label: 'Leaderboard' },
   { id: 'market', label: 'Market' },
-  { id: 'leaderboard', label: 'Leaderboard' },
 ])
-const isActive = ref('market')
+const isActive = ref('')
 
 const setActive = (id) => {
   isActive.value = id
@@ -47,8 +49,59 @@ const setActive = (id) => {
   flex-direction: row;
   justify-content: center;
   align-items: center;
+  margin: 10px 5px;
+  padding: 10px 20px;
+  gap: 15px;
+  background-color: $color-white;
+  box-shadow: $box-shadow-nav;
+  border-radius: 20px;
+  &__option {
+    list-style: none;
+    margin: 10px 0;
+    &--active {
+      background-color: $color-primary-background;
+      border: 1px solid $color-primary-background;
+      color: $color-white;
+    }
+  }
+  &__link {
+    text-decoration: none;
+    transition: color 0.3s ease;
+    background-color: $color-white;
+    border: 1px solid $color-primary-background;
+    color: $color-primary-background;
+    padding: 12px;
+    border-radius: 20px;
+    font-weight: bold;
+    font-size: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &:hover {
+      background-color: $color-primary-background;
+      border: 1px solid $color-primary-background;
+      color: $color-white;
+      svg {
+        fill: $color-white;
+      }
+    }
+    &--active {
+      background-color: $color-primary-background;
+      border: 1px solid $color-primary-background;
+      color: $color-white;
+      &:hover {
+        background-color: $color-primary-background-hover;
+        border-color: $color-primary-background-hover;
+      }
+    }
+  }
 }
-.nav-app-container__option {
-  background-color: --color-background;
+.icon-rank {
+  width: 20px;
+  fill: $color-primary-background;
+  margin-right: 2px;
+  &--active {
+    fill: $color-white;
+  }
 }
 </style>
