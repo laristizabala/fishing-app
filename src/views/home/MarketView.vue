769fec609f533171e537985d5ea165cc8259caf7
laristizabala <laura.aristizabal@guane.com.co>
2025-05-02 19:05:09 -0500

    :lipstick: styles marketView improved

--- src/views/home/MarketView.vue
+++ src/views/home/MarketView.vue
@@ -9,13 +9,32 @@
         </p>
       </div>
       <div class="market-card-body" v-for="item in market?.items" :key="item.id">
+        <div class="market-card-body__item">
+          <div class="market-card-body__item-icon">
+            <IconFishingRod
+              v-if="item.type === 'fishing_rod'"
+              class="icon-general"
+            ></IconFishingRod>
+            <IconPoisonDelay
+              v-if="item.type === 'poison_delay' || item.type === 'poison_leveling'"
+              class="icon-general"
+            ></IconPoisonDelay>
+            <IconPoisonRecovery
+              v-if="item.type === 'poison_recovery'"
+              class="icon-general"
+            ></IconPoisonRecovery>
+            <IconFish v-if="item.type === 'poison_reveal_fishes'" class="icon-general"></IconFish>
+          </div>
+          <p class="market-card-body__text market-card-body__text--main">
+            {{ item.name }}
+          </p>
+        </div>
         <p class="market-card-body__text">
-          {{ item.name }}
-        </p>
-        <p class="market-card-body__text">
+          <span class="market-card-body__label">Type: </span>
           {{ item.type }}
         </p>
         <p class="market-card-body__text">
+          <span class="market-card-body__label">Description: </span>
           {{ item.description }}
         </p>
       </div>
@@ -27,6 +46,10 @@
 import { ref, onMounted } from 'vue'
 import AppLoading from '../../components/AppLoading.vue'
 import { marketDummyData } from '../../utils/dummyData'
+import IconFishingRod from '../../components/icons/IconFishingRod.vue'
+import IconPoisonDelay from '../../components/icons/IconPoisonDelay.vue'
+import IconPoisonRecovery from '../../components/icons/IconPoisonRecovery.vue'
+import IconFish from '../../components/icons/IconFish.vue'
 
 const market = ref({})
 const loading = ref(false)
@@ -79,6 +102,7 @@ onMounted(() => {
     font-size: 0.875rem;
     font-weight: bold;
     color: $color-secondary-background;
+    text-align: center;
   }
 }
 .market-card-body {
@@ -90,11 +114,21 @@ onMounted(() => {
   border-bottom: 0.063rem solid $color-gray-hr;
   padding: 0.625rem 0 0.625rem 0.875rem;
   margin: 0.625rem 0;
+  &__label {
+    display: none;
+  }
   &__text {
     display: flex;
     align-items: center;
+    justify-content: center;
     color: $color-text-soft;
     font-size: 0.75rem;
+
+    &--main {
+      font-weight: bold;
+      color: $color-secondary-background;
+      text-align: center;
+    }
   }
   &__rank {
     background-color: $color-secondary-background;
@@ -108,6 +142,63 @@ onMounted(() => {
     padding: 0.125rem;
     margin: 0.125rem;
     color: $color-white;
+  }
+  &__item {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  &__item-icon {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    border: 0.125rem solid $color-primary-background;
+    border-radius: 2.5rem;
+    padding: 0.6rem;
+  }
+}
+
+.icon-general {
+  width: 2rem;
+  height: 2rem;
+  fill: $color-primary-background;
+  margin-right: 0.125rem;
+}
+
+@media (max-width: 580px) {
+  .market-card-header {
+    display: none;
+  }
+
+  .market-card-body {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: 1fr;
+    padding: 1rem;
+    text-align: center;
+    border: 0.063rem solid $color-gray-hr;
+    border-radius: 1rem;
+    &__label {
+      display: block;
+      font-weight: 600;
+      color: $color-primary-background;
+      margin-right: 0.188rem;
+    }
+    &__text {
+      flex-direction: column;
+    }
+  }
+}
+@media (max-width: 400px) {
+  .market-card-body {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    &__text {
+      flex-direction: row;
+    }
   }
 }
 </style>
